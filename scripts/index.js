62de308fed03710b8fb0bda6de503995a314e104
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2020-08-13 09:10:54 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const fixNumber = 0;
+
+let operationNumber = fixNumber;
+
+
+
+let firstNumber = 2;
+let secondNumber = 6;
+
+operationNumber = add(firstNumber, secondNumber);
+console.log(operationNumber);
+
+
+
+
+function add(first, second) {
+    result = first + second;
+    return result;
+}
